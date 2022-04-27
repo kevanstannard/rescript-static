9d4b81e55338089476e8a478e2af2223439458b7
Kevan Stannard <kevanstannard@gmail.com>
2022-04-28 07:44:41 +1000

    Fix static file usage

--- scripts/serve.js
+++ scripts/serve.js
@@ -15,7 +15,6 @@ const options = {
   watch: true, // Watch for changes in the <dir> directory
   open: false, // Don't open a browser window on start up
   notify: false, // Don't show browser sync notifications in the browser
-  serveStatic: ["./static"], // Both /image.jpeg and /static/image.jpeg will work
 };
 
 const server = browserSync.create("rescript-static");
